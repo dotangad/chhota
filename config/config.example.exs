70bb4328d9f6a805e372a3b7a07f2ce08fabfdfa
Angad Singh <mail@angad.dev>
2020-09-01 18:39:20 +0530

    Setup config

---
+++ config/config.example.exs
@@ -0,0 +1,8 @@
+import Config
+
+config :chhota,
+  base_website: "https://angad.dev",
+  github_username: "dotangad",
+  airtable_api_key: ""
+  table_key: ""
+  table_name: ""
--- mix.exs
+++ mix.exs
@@ -22,7 +22,8 @@ defmodule Chhota.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:airtable, "~> 0.4.0"}
     ]
   end
 end
